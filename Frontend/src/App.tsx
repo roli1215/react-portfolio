b24f7b921bb79581258b55d5d01431302afa74e8
ROLI\user <karczubroland@gmail.com>
2024-09-03 15:52:06 +0200

    default sidenav

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
 
+import Sidenav from './components/SideNav'
 function App() {
 
   return (
     <>
       <div>
+        <Sidenav />
       </div>
     </>
   )
--- Frontend/src/components/Sidenav.tsx
+++ Frontend/src/components/Sidenav.tsx
@@ -1,9 +1,39 @@
+import { AiOutlineMail, AiOutlineHome, AiOutlineMenu } from "react-icons/ai";
+import React, { useState } from "react";
+import { GrProjects } from "react-icons/gr";
 
 const Sidenav = () => {
+
+    const [nav, setNav] = useState(false);
+    const handleNav = () => {
+        setNav(!nav);
+    };
+
     return (
         <div>
-        Sidenav
+            <AiOutlineMenu onClick={handleNav} className = 'absolute top-4 right-4 z-[99] md:hidden'/>
+            { 
+                nav ? (
+                    <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
+                        <a href="#home" className = "w-[75%] flex justify-center  items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration:200">
+                            <AiOutlineHome size={20} />
+                            <span className='pl-4'>Home</span>
+                        </a>
+                        <a href="#home" className = "w-[75%] flex justify-center  items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration:200">
+                            <GrProjects size={20} />
+                            <span className='pl-4'>Projects</span>
+                        </a>
+                        <a href="#home" className = "w-[75%] flex justify-center  items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration:200">
+                            <AiOutlineMail size={20} />
+                            <span className='pl-4'>Contact</span>
+                        </a>
+                    </div>
+                ) :
+                (
+                    ''
+                )
+            }
         </div>
-    );
+        );
     }
 export default Sidenav;
