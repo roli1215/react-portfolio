9f4c22a1bc0da762b67049cc68ee6420187210ed
ROLI\user <karczubroland@gmail.com>
2024-09-04 17:05:02 +0200

    working on projects cover

--- Frontend/src/components/ProjectItem.tsx
+++ Frontend/src/components/ProjectItem.tsx
@@ -2,20 +2,20 @@
 interface ProjectItemProps {
     img : string,
     title: string;
+    description : string;
+    language: string
     }
 
-const ProjectItem = ({title, img} : ProjectItemProps) => {
+const ProjectItem = ({title, img, description,language} : ProjectItemProps) => {
   return (
-    <div className = "relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-600 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
+    <div className = "relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-600 rounded-xl group hover:bg-gradient-to-r from-gray-800 to-[#2b2a2a]">
     <img src={img} alt="/" className="rounded-xl group-hover:opacity-10"/>
-    <div className = "hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-        <h3 className="text-2xl font-bold text-white tracking-wider text-center">
+    <div className = "hidden group-hover:block absolute top-[50%] left-[15%] translate-x-[-10%] translate-y-[-50%]">
+        <h3 className="text-2xl font-bold text-white tracking-wider text-center underline">
             {title}
         </h3>
-        <p className="pb-4 pt-2 text-white text-center">React Js</p>
-        <a href="/">
-            <p className="text-center p-3 rounded-ld bg-white text-gray-700 font-bold cursor-pointer text-lg">More info</p>
-        </a>
+        <h2 className="text-center font-bold m-3 text-base text-gray-300">{language}</h2>
+        <h2 className="font-bold m-3 text-justify text-sm text-gray-300 italic" >{description}</h2>
     </div>
     </div>
   )
