7d82c27ab5d1f324f36b8d78519a8f66fb16d558
ROLI\user <karczubroland@gmail.com>
2024-09-09 18:59:18 +0200

    try to build backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,16 @@
-const world = 'world';
 
-export function hello(who: string = world): string {
-  return `Hello ${who}! `;
-}
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/portfolio');
+
+app.get('/getUsers', (req: Express.Request, res: Express.Response) => {
+});
+
+ 
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
