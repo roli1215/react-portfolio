b8f85eee1bb78800bb6cfd3c3ea9abca5210ff95
ROLI\user <karczubroland@gmail.com>
2024-09-10 20:44:30 +0200

    frontend - backend form post ok

--- Frontend/src/screens/Contact.tsx
+++ Frontend/src/screens/Contact.tsx
@@ -1,32 +1,69 @@
+import axios from "axios";
+import { useState } from "react";
 
 const Contact = () => {
+
+    const [formData, setFormData] = useState({
+        name : '',
+        phone : '',
+        email : '',
+        subject : '',
+        message : '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setFormData({
+            ...formData,
+            [e.target.name] : e.target.value
+        });
+    }
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:3000/applies', formData);
+            console.log(response.data.message);
+            setFormData({
+                name : '',
+                phone : '',
+                email : '',
+                subject : '',
+                message : '',
+            });
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
+
   return (
     <div id="contact" className="max-w-[1200px] m-auto md:pl-20 p-4 ">
         <h1 className ="py-4 text-4xl font-bold text-center text-[#000] ">Contact</h1>
-        <form action ="" method="POST" encType="multipart/form-data">
+        <form onSubmit={handleSubmit} method="POST" encType="multipart/form-data">
             <div className="grid md:grid-cols-2 gap-4 w-full py-2">
                 <div className="flex flex-col">
                     <label className="uppercase text-sm py-2 font-bold">Name</label>
-                    <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" name="name" />
+                    <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" name="name" value={formData.name} onChange={handleChange} required />
                 </div>
                 <div className="flex flex-col">
                     <label  className="uppercase text-sm py-2 font-bold">Phone</label>
-                    <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" name="phone" />
+                    <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" name="phone" value={formData.phone} onChange={handleChange} required />
                 </div>
             </div>
             <div className="flex flex-col py-2">
                 <label  className="uppercase text-sm py-2 font-bold">Email</label>
-                <input className="border-2 rounded-lg p-3 flex border-gray-300" type="email" name="email" />
+                <input className="border-2 rounded-lg p-3 flex border-gray-300" type="email" name="email" value={formData.email} onChange={handleChange} required />
             </div>
             <div className="flex flex-col py-2">
                 <label  className="uppercase text-sm py-2 font-bold">Subject</label>
-                <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" name="subject" />
+                <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" name="subject" value={formData.subject} onChange={handleChange} required />
             </div>
             <div className="flex flex-col py-2">
                 <label  className="uppercase text-sm py-2 font-bold">Message</label>
-                <textarea className="border-2 rounded-lg p-3 border-gray-300 " rows={10} name="message"></textarea>
+                <textarea className="border-2 rounded-lg p-3 border-gray-300 " rows={10} name="message" value={formData.message} onChange={handleChange} required ></textarea>
             </div>
-            <button className="bg-[#000] text-gray-100 mt-4 w-full p-4 rounded-lg font-bold">
+            <button className="bg-[#000] text-gray-100 mt-4 w-full p-4 rounded-lg font-bold" type="submit">
                 Send Message
             </button>
         </form>
