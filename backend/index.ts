62c48a71aba337e65cb9b10bf966088d7c920b51
ROLI\user <karczubroland@gmail.com>
2024-09-10 20:11:17 +0200

    default backend structure

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,34 @@
-
-const express = require('express');
-const mongoose = require('mongoose');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
-mongoose.connect('mongodb://localhost:27017/portfolio');
+const PORT = process.env.PORT || 5000;
+const MONGOURL = process.env.MONGO_URL;
 
-app.get('/getUsers', (req: Express.Request, res: Express.Response) => {
+if (!MONGOURL) {
+  throw new Error('MONGO_URL is not defined in the environment variables');
+}
+
+mongoose.connect(MONGOURL).then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}).catch((error) => {
+  console.error('Error connecting to MongoDB', error);
 });
 
- 
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const UserModel = mongoose.model('datas', userSchema);
+
+app.get('/datas', async (req, res) => {
+  const users = await UserModel.find();
+  res.json(users);
 });
