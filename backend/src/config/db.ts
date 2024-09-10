c4f78ff660e362f56a9fdd27267519286b64559e
ROLI\user <karczubroland@gmail.com>
2024-09-10 20:14:25 +0200

    db connection

---
+++ backend/src/config/db.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+      const MONGO_URL = process.env.MONGO_URL;
+      if (!MONGO_URL) {
+        throw new Error('MONGO_URL is not defined in the environment variables');
+      }
+  
+      await mongoose.connect(MONGO_URL);
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+      process.exit(1); 
+    }
+  };
+  
+  export default connectDB;
