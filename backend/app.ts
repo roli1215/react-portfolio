a96bcff9da083e4c6921708f7d14d2027d803a59
ROLI\user <karczubroland@gmail.com>
2024-09-10 20:20:41 +0200

    server, app and middlewares

---
+++ backend/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import appliesRoutes from './src/routes/appliesRoutes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(appliesRoutes);
+
+export default app;
--- backend/index.ts
+++ backend/index.ts
@@ -1,34 +1,13 @@
-import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import app from './app';
+import connectDB from './src/config/db';
 
-const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const MONGOURL = process.env.MONGO_URL;
 
-if (!MONGOURL) {
-  throw new Error('MONGO_URL is not defined in the environment variables');
-}
-
-mongoose.connect(MONGOURL).then(() => {
-  console.log('Connected to MongoDB');
+connectDB().then (() => {
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });
-}).catch((error) => {
-  console.error('Error connecting to MongoDB', error);
-});
-
-const userSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-});
-
-const UserModel = mongoose.model('datas', userSchema);
-
-app.get('/datas', async (req, res) => {
-  const users = await UserModel.find();
-  res.json(users);
 });
