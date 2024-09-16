d4cdd70ca8c9f47444d9e482d2c724b2ef09a332
ROLI\user <karczubroland@gmail.com>
2024-09-16 18:33:51 +0200

    update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
-import app from './app';
-import connectDB from './src/config/db';
+import app from '../app';
+import connectDB from './config/db';
 
 dotenv.config();
 
