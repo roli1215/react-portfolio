6bb1ec64fbc45fbf8aba13ec569de9586f4e6e40
ROLI\user <karczubroland@gmail.com>
2024-09-16 21:49:49 +0200

    back to default, later next

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -7,24 +7,6 @@ import About from './screens/About'
 
 function App() {
 
-
-  function handleClick() {
-    if (localStorage.theme === "dark" || !("theme" in localStorage)) {
-      //add class=dark in html element
-      document.documentElement.classList.add("dark");
-    } else {
-      //remove class=dark in html element
-      document.documentElement.classList.remove("dark");
-    }
-
-    if (localStorage.theme === "dark") {
-      localStorage.theme = "light";
-    } else {
-      localStorage.theme = "dark";
-    }
-  }
-
-
   return (
     <>
       <div>
@@ -33,11 +15,6 @@ function App() {
         <Projects />
         <About />
         <Contact />
-        <div className="flex" onClick={handleClick}>
-        <div className="flex-initial w-64 font-bold underline decoration-sky-500 text-red-800">
-          Change Mode (Dark/Light)
-        </div>
-      </div>
       </div>
     </>
   )
