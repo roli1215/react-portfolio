1234505f56c4b28674c37a32d1cf53b6e352b1de
ROLI\user <karczubroland@gmail.com>
2024-09-17 20:31:41 +0200

    titles set to left, and set border under them

--- Frontend/src/screens/About.tsx
+++ Frontend/src/screens/About.tsx
@@ -3,7 +3,8 @@ import SkillsItem from '../components/SkillsItem'
 const About = () => {
   return (
     <div id='about' className='max-w-[1200px] m-auto md:pl-20 p-4 py-16'>
-      <h1 className='text-4xl font-bold text-center text-[#000000] '>About</h1>
+      <h1 className='text-4xl font-bold text-left text-[#000000] '>About</h1>
+      <div className="w-full h-[2px] bg-black mt-2"></div>
       <p className="font-bold py-8 text-justify">I am a graduate software developer with a strong interest in web development and mobile app development. I have created a few personal projects so far, some of which are still in progress, as I strive to improve and reach a higher level of proficiency. My experience is primarily in React/React Native, working with JavaScript/TypeScript, and in database management with MongoDB and PostgreSQL. I am open to trying new things and am committed to performing the tasks ahead of me to the best of my ability.</p>
       <h1 className='text-3xl font-bold  text-[#000000] '>Education</h1>
         <div className="w-full h-[2px] bg-black mt-2"></div>
@@ -16,6 +17,7 @@ const About = () => {
               <span>2017-2021</span>
             </div>
             <h1 className='text-3xl font-bold text-[#000000] py-8'>Experience</h1>
+            <div className="w-full h-[2px] bg-black mt-2"></div>
             <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-8'>
             <SkillsItem img="assets/react.svg" ></SkillsItem>
             <SkillsItem img="assets/typescript.svg" ></SkillsItem>
