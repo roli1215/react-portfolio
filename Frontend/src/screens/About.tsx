a699f8b80cfec28e374dc7e3df6d03cb7d4af735
ROLI\user <karczubroland@gmail.com>
2024-09-17 20:28:00 +0200

    years position edit in about

--- Frontend/src/screens/About.tsx
+++ Frontend/src/screens/About.tsx
@@ -7,12 +7,16 @@ const About = () => {
       <p className="font-bold py-8 text-justify">I am a graduate software developer with a strong interest in web development and mobile app development. I have created a few personal projects so far, some of which are still in progress, as I strive to improve and reach a higher level of proficiency. My experience is primarily in React/React Native, working with JavaScript/TypeScript, and in database management with MongoDB and PostgreSQL. I am open to trying new things and am committed to performing the tasks ahead of me to the best of my ability.</p>
       <h1 className='text-3xl font-bold  text-[#000000] '>Education</h1>
         <div className="w-full h-[2px] bg-black mt-2"></div>
-            <p className="text-right font-bold">2021-2024</p>
-            <div className="font-bold">Software Development – University of Miskolc</div>
-      
-      <div className="font-bold">Information Technology Major – István Bocskai High School and Technical School <p className="text-right font-bold">2017-2021</p></div>
+            <div className="flex justify-between font-bold">
+              <span>Software Development – University of Miskolc</span>
+              <span>2021-2024</span >
+            </div>
+            <div className="flex justify-between font-bold">
+              <span>Information Technology Major – István Bocskai High School and Technical School</span>
+              <span>2017-2021</span>
+            </div>
             <h1 className='text-3xl font-bold text-[#000000] py-8'>Experience</h1>
-        <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-8'>
+            <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-8'>
             <SkillsItem img="assets/react.svg" ></SkillsItem>
             <SkillsItem img="assets/typescript.svg" ></SkillsItem>
             <SkillsItem img="assets/javascript.svg" ></SkillsItem>
@@ -27,7 +31,7 @@ const About = () => {
             <SkillsItem img="assets/swagger.svg" ></SkillsItem>
             <SkillsItem img="assets/git.svg" ></SkillsItem>
             <SkillsItem img="assets/html.svg" ></SkillsItem>
-        </div>
+            </div>
     </div>
   )
 }
