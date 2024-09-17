2a912f89598ad05cb7fee4f94f539d535036ecbd
ROLI\user <karczubroland@gmail.com>
2024-09-17 20:40:39 +0200

    projectitem and about page correcting

--- Frontend/src/screens/About.tsx
+++ Frontend/src/screens/About.tsx
@@ -8,11 +8,11 @@ const About = () => {
       <p className="font-bold py-8 text-justify">I am a graduate software developer with a strong interest in web development and mobile app development. I have created a few personal projects so far, some of which are still in progress, as I strive to improve and reach a higher level of proficiency. My experience is primarily in React/React Native, working with JavaScript/TypeScript, and in database management with MongoDB and PostgreSQL. I am open to trying new things and am committed to performing the tasks ahead of me to the best of my ability.</p>
       <h1 className='text-3xl font-bold  text-[#000000] '>Education</h1>
         <div className="w-full h-[2px] bg-black mt-2"></div>
-            <div className="flex justify-between font-bold">
+            <div className="flex justify-between font-bold py-4">
               <span>Software Development – University of Miskolc</span>
               <span>2021-2024</span >
             </div>
-            <div className="flex justify-between font-bold">
+            <div className="flex justify-between font-bold py-4">
               <span>Information Technology Major – István Bocskai High School and Technical School</span>
               <span>2017-2021</span>
             </div>
