a9f5ae903ab4258ba59b27a7be78955d3a8707e2
ROLI\user <karczubroland@gmail.com>
2024-09-30 21:52:16 +0200

    backend send email ok

--- backend/src/controllers/appliesController.ts
+++ backend/src/controllers/appliesController.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from 'express';
 import ApplyModel from '../models/applyModel';
+import sgMail from '@sendgrid/mail';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 
 export const getApplies = async (req: Request, res: Response) => {
   try {
@@ -10,9 +16,9 @@ export const getApplies = async (req: Request, res: Response) => {
   }
 };
 
-export const postApply = async (req : Request, res: Response) => {
+export const postApply = async (req: Request, res: Response) => {
   try {
-    const { name, phone, email ,subject, message } = req.body;
+    const { name, phone, email, subject, message } = req.body;
 
     const newApply = new ApplyModel({
       name,
@@ -23,9 +29,22 @@ export const postApply = async (req : Request, res: Response) => {
     });
 
     await newApply.save();
-    res.status(201).json({ message: 'Apply created successfully' });
 
+    const msg = {
+      to: 'karczubroland@gmail.com', 
+      from: 'karczubroland@gmail.com',
+      subject: `Szeretné veled a kapcsolatot felvenni: ${name}`,
+      text: `Új megkeresés érkezett, űrlap adatai:\n\nNév: ${name}\nTelefonszám: ${phone}\nEmail-cím: ${email}\nTárgy: ${subject}\nÜzenet:\n${message}`,
+    };
+
+    try {
+      await sgMail.send(msg);
+      res.status(201).json({ message: 'Apply created and email sent successfully' });
+    } catch (emailError) {
+      console.error('Error sending email:', emailError);
+      res.status(500).json({ message: 'Apply created, but email failed to send', emailError });
+    }
   } catch (error) {
     res.status(500).json({ message: 'Error creating apply', error });
   }
-}
+};
