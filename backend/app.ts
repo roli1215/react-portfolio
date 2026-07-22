ae302dedde09746137974be33acc1ec583fd1e49
ROLI\user <karczubroland@gmail.com>
2026-07-22 21:40:39 +0200

    Auth setup

--- backend/app.ts
+++ backend/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import appliesRoutes from './src/routes/appliesRoutes';
 import projectRoutes from './src/routes/projectRoutes';
+import authRoutes from './src/routes/authRoutes';
 
 const app = express();
 
@@ -9,6 +10,10 @@ app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
+app.use(
+    "/auth",
+    authRoutes
+);
 app.use(appliesRoutes);
 app.use(projectRoutes);
 export default app;
---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+export const login = (
+    req: Request,
+    res: Response
+) => {
+
+    const {
+        username,
+        password
+    } = req.body;
+
+
+    if (
+        username !== process.env.ADMIN_USERNAME ||
+        password !== process.env.ADMIN_PASSWORD
+    ) {
+        return res.status(401).json({
+            message: "Invalid credentials"
+        });
+    }
+
+    const token = jwt.sign(
+        {
+            username
+        },
+        process.env.JWT_SECRET!,
+        {
+            expiresIn: "2h"
+        }
+    );
+
+    res.json({
+        token
+    });
+
+};
